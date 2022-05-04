b543753fd362b82a4dc35d250493015165de3156
Célien JACQUEMIER <celien.jacquemier@Digital-Grenoble.local>
2022-05-04 11:58:59 +0200

    iteration 3.3

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -1,1 +1,13 @@
 <?php
+$book = ["name" => "Book",
+    "price" => 1000,
+    "weight" => 100,
+    "discount" => 10,
+    "picture_url" => "https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png",];
+?>
+
+<div>
+<h3>Much book</h3>
+<p>Prix : 1000€</p>
+<img src="https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png">
+</div>
